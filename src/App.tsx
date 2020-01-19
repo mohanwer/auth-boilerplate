51f0b27093c07d538d9b7ae90c63a66be424ba37
mohanwer <mohrocks4@gmail.com>
2020-01-19 15:01:59 -0500

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Provider} from 'react-redux'
+import { ConnectedRouter } from "connected-react-router"
+import { createBrowserHistory } from "history"
+import {Route, Switch} from "react-router"
+import axios from 'axios'
+import './App.css'
+import {configureStore} from "./store/store"
+import {Home} from './Home'
+import {General} from "./General"
+
+const history = createBrowserHistory();
+const store = configureStore(history, {});
+const token = localStorage.getItem('token');
+
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Authorization ${token}`;
+} else {
+  axios.defaults.headers.common['Authorization'] = null;
+}
+axios.interceptors.response.use((response) => response.data)
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route component={Home} path={'/Home'}/>
+          <Route component={General} path={'/General'}/>
+        </Switch>
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
-export default App;
+export default App
---
+++ src/General.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const General = () => (<div>General page</div>)
